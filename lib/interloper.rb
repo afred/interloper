c37b11a77d48719142f0547af3e7b9d766a4e0cb
Andrew Myers <afredmyers@gmail.com>
2017-10-20 12:09:57 -0600

    Implements the Interloper module
    Allows defining a stack of 'before' and 'after' callbacks for methods on POROs.

--- lib/interloper.rb
+++ lib/interloper.rb
@@ -1,13 +1,100 @@
 require "interloper/version"
 
 module Interloper
-  def before(*method_names, &callback)
-    callback_layer_module.define_methods_with_hooks(*method_names)
-    callback_layer_module.add_before_callbacks(*method_names, &callback)
+
+  def interloper_module
+    self.class.interloper_module
   end
 
-  def after(*method_names, &callback)
-    callback_layer_module.define_methods_with_hooks(*method_names)
-    callback_layer_module.add_after_callbacks(*method_names, &callback)
+  def Interloper.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+
+    # Generates an Interloper module that is namespeced under the including
+    # class, if one does not already exist. Then prepends the Interloper
+    # module to the including class.
+    # @return Module the Interloper module that was prepended to the including
+    #   class.
+    def interloper_module
+      # Create the Interloper module if it doesn't exist already
+      const_set(:Interloper, generate_interloper_module) unless self.constants.include? :Interloper
+      # Prepend the interloper module
+      prepend const_get(:Interloper)
+      const_get(:Interloper)
+    end
+
+    def generate_interloper_module
+      Module.new do
+        class << self
+          def before_callbacks
+            @before_callbacks ||= {}
+          end
+
+          def after_callbacks
+            @after_callbacks ||= {}
+          end
+
+          def before_callbacks_for(method_name)
+            before_callbacks[method_name] || []
+          end
+
+          def after_callbacks_for(method_name)
+            after_callbacks[method_name] || []
+          end
+
+          def run_all_before_callbacks_for(method_name, object_context, *orig_args, &orig_block)
+            before_callbacks_for(method_name).each do |callback|
+              # object_context.instance_eval(callback.call(*orig_args,&orig_block))
+              object_context.instance_eval &callback
+            end
+          end
+
+          def run_all_after_callbacks_for(method_name, object_context, *orig_args, &orig_block)
+            after_callbacks_for(method_name).each do |callback|
+              object_context.instance_eval(callback.call(*orig_args,&orig_block))
+            end
+          end
+
+
+          def add_before_callbacks(*method_names, &callback)
+            method_names.each do |method_name|
+              before_callbacks[method_name] ||= []
+              before_callbacks[method_name] << callback
+            end
+          end
+
+          def add_after_callbacks(*method_names, &callback)
+            method_names.each do |method_name|
+              after_callbacks[method_name] ||= []
+              after_callbacks[method_name] << callback
+            end
+          end
+
+          def define_interloper_methods(*method_names)
+            method_names.each do |method_name|
+              define_method(method_name) do |*args, &block|
+                called_method = __method__
+                interloper_module.run_all_before_callbacks_for(called_method, self, *args, &block)
+                return_val = super(*args,&block)
+                interloper_module.run_all_after_callbacks_for(called_method, self, *args, &block)
+                return_val
+              end
+            end
+          end
+        end
+      end
+    end
+
+    def before(*method_names, &callback)
+      interloper_module.define_interloper_methods(*method_names)
+      interloper_module.add_before_callbacks(*method_names, &callback)
+    end
+
+    def after(*method_names, &callback)
+      interloper_module.define_interloper_methods(*method_names)
+      interloper_module.add_after_callbacks(*method_names, &callback)
+    end
   end
 end
--- spec/interloper_spec.rb
+++ spec/interloper_spec.rb
@@ -1,23 +1,59 @@
 require "spec_helper"
 
 RSpec.describe Interloper do
+
+  # Factory method for creating test classes that include Interloper by
+  # default, and can be configured further with a block.
+  def test_class_factory(&block)
+    Class.new do
+      include Interloper
+    end.tap do |klass|
+      klass.class_eval(&block) if block_given?
+    end
+  end
+
+  let(:test_class) { test_class_factory }
+
   it "has a version number" do
     expect(Interloper::VERSION).not_to be nil
   end
 
   context 'when extended by a class' do
-    before :all do
-      class TestClass
-        extend Interloper
-      end
-    end
+    subject { test_class }
 
     context 'provides a class interface:' do
-      subject { TestClass }
       it { is_expected.to respond_to(:before) }
       it { is_expected.to respond_to(:after) }
     end
+
+    context 'when a before hook is configured for a method' do
+
+      let(:test_class) do
+        test_class_factory do
+          # Add callback to run before :do_something
+          before(:do_something) { before_do_something }
+
+          # Define the callback here so we can test to see if it's called.
+          def before_do_something; end
+
+          # Define the method we're adding callbacks for.
+          def do_something
+            # Call a method tha we can test to see if/when it's been called.
+            observable_action
+          end
+
+          # Define a method that we can test to see it it's called.
+          def observable_action; end
+        end
+      end
+
+      let(:test_instance) { test_class.new }
+
+      it 'calls the hook before the method is called' do
+        expect(test_instance).to receive(:before_do_something).exactly(1).times.ordered
+        expect(test_instance).to receive(:observable_action).exactly(1).times.ordered
+        test_instance.do_something
+      end
+    end
   end
-
-
 end
